2946efa046d0d123c77addd20a005267528bf409
mcarbonell <dontspam@gmail.com>
2024-05-24 20:26:40 +0200

    manifest tabs permision and use

--- script.js
+++ script.js
@@ -2,5 +2,6 @@
     // see the note below on how to choose currentWindow or lastFocusedWindow
     const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
     console.log(tab.url);
+    document.getElementById('currurl').textContent = tab.url;
     // ..........
   })();
